d879aff2968bf74729e36179983a8c74e1ccc07f
Iswenzz <suxlolz1528@gmail.com>
2022-01-31 19:48:58 +0100

    Text: Inherit size

--- packages/izui-react/src/components/Content/Text/Text.tsx
+++ packages/izui-react/src/components/Content/Text/Text.tsx
@@ -2,11 +2,14 @@ import { memo, FC, ElementType } from "react";
 import { useTranslation } from "react-i18next";
 import { Typography, TypographyProps } from "@mui/material";
 import { v4 as uuidv4 } from "uuid";
+import classNames from "classnames";
+
+import scss from "./Text.module.scss";
 
 /**
  * Typography wrapper for multiple strings.
  */
-const Text: FC<TextProps> = ({ children, raw, component = "h6", ...rest }) =>
+const Text: FC<TextProps> = ({ children, className, raw, component = "h6", ...rest }) =>
 {
 	const { t } = useTranslation();
 	const items = Array.isArray(children) ? children : [children];
@@ -15,7 +18,8 @@ const Text: FC<TextProps> = ({ children, raw, component = "h6", ...rest }) =>
 		<>
 			{items.map(item => (
 				!raw ? (
-					<Typography {...rest} component={component} key={uuidv4()}>
+					<Typography {...rest} className={classNames(scss.text, className)}
+						component={component} key={uuidv4()}>
 						{t(item)}
 					</Typography>
 				) : t(item)
