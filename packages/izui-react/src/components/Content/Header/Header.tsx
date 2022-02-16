3e68d772f4a1da654d88622e3842b74befc235a2
Iswenzz <suxlolz1528@gmail.com>
2022-02-16 18:39:10 +0100

    Styles: Colors + Alpha

--- packages/izui-react/src/components/Content/Header/Header.tsx
+++ packages/izui-react/src/components/Content/Header/Header.tsx
@@ -10,17 +10,20 @@ import scss from "./Header.module.scss";
  * Header with a parallax background & title.
  */
 const Header: FC<HeaderProps> = ({
-	children, className, title, description = "", background, parallaxStrength = -30, parallaxBlur = 0
+	children, className, titleClassName, descriptionClassName, title, description = "",
+	background, parallaxStrength = -30, parallaxBlur = 0
 }) => (
 	<header>
 		<Parallax className={classNames(scss.parallax, className)} image={background}
 			blur={parallaxBlur} speed={parallaxStrength}>
 			<Grid component="section" container className={scss.grid}
 				direction="column" justifyContent="center" alignItems="center">
-				<TrailText className="calli-h1 bold noselect" align="center" variant="h1" component="h1">
+				<TrailText className={classNames("calli-big bold noselect", titleClassName)}
+					align="center" variant="h1" component="h1">
 					{title}
 				</TrailText>
-				<TrailText className="poiret bold noselect" align="center" variant="h3" component="h3">
+				<TrailText className={classNames("poiret bold noselect", descriptionClassName)}
+					align="center" variant="h3" component="h3">
 					{description}
 				</TrailText>
 				{children}
@@ -32,6 +35,8 @@ const Header: FC<HeaderProps> = ({
 type HeaderProps = {
 	children?: ReactElement,
 	className?: string,
+	titleClassName?: string,
+	descriptionClassName?: string,
 	title: string,
 	description?: string,
 	background: string,
