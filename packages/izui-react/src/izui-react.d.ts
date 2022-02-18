ae8831ae8dae645cb30b58e0c1dc4f061a9a2a49
Iswenzz <suxlolz1528@gmail.com>
2022-02-18 16:41:23 +0100

    Types: mui-material

--- packages/izui-react/src/izui-react.d.ts
+++ packages/izui-react/src/izui-react.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="@izui/types/global" />
-/// <reference types="./types/mui-material" />
+/// <reference types="@izui/types/mui-material" />
 
 export * from "./components";
 export * from "./router";
